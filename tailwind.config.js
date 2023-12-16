b96f9bc0c92a393d386b5ee5a0e1176419c7c949
Łukasz Bacik <mail@luka.sh>
2023-12-16 09:30:51 +0100

    Base css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class', '[data-mode="dark"]'],
   content: [
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
     "./vendor/tales-from-a-dev/flowbite-bundle/templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'width': {
+        '20px': '20px',
+      }
+    },
   },
   plugins: [],
 }
